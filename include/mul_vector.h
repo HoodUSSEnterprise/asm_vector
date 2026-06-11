78da0af5fbdc81318ed6586f0eb8b3a14f2e21fe
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-11 09:14:58 +0800

    add: mul vector head file

---
+++ include/mul_vector.h
@@ -0,0 +1,10 @@
+#ifndef MUL_VECTOR_H
+#define MUL_VECTOR_H
+
+#include "myvector.h"
+
+int mul_vector_dot(MyVector *v1, MyVector *v2);
+
+MyVector *mul_vector_cross(MyVector *v1, MyVector *v2);
+
+#endif // MUL_VECTOR_H
