cceb4e30af7a1e778c685df33246c2881ff6c656
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-11 13:36:40 +0800

    add: remove_vector head file

---
+++ include/remove_vector.h
@@ -0,0 +1,9 @@
+#ifndef REMOVE_VECTOR_H
+#define REMOVE_VECTOR_H
+
+#include "find_vector.h"
+#include "myvector.h"
+
+bool remove_vector(MyVector *v, int removed_value);
+
+#endif // REMOVE_VECTOR_H
