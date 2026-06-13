f53d564f045c2703b72476d790bbe0ce0eec3910
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 11:20:17 +0800

    change:MyVector to VectorInt

--- include/mul_vector.h
+++ include/mul_vector.h
@@ -3,8 +3,8 @@
 
 #include "myvector.h"
 
-int mul_vector_dot(VectorInt *v1, VectorInt *v2);
+int imul_vector_int_dot(VectorInt *v1, VectorInt *v2);
 
-VectorInt *mul_vector_cross(VectorInt *v1, VectorInt *v2);
+VectorInt *mul_vector_int_cross(VectorInt *v1, VectorInt *v2);
 
 #endif // MUL_VECTOR_H
