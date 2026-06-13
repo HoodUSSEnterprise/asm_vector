8b508d9ad3e23acf0d2820b48d497b9d1c50b23b
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 10:18:25 +0800

    change: struct name from MyVector into VectorInt

--- src/sub_vector.asm
+++ src/sub_vector.asm
@@ -3,7 +3,7 @@ section .text
 extern malloc
 extern free
 
-;MyVector *sub_vector_int(MyVector *v1, MyVector *v2);
+;VectorInt *sub_vector_int(VectorInt *v1, VectorInt *v2);
 ;rcx = v1, rdx = v2
 sub_vector_int:
 
