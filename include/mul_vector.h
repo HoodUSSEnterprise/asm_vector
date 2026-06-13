47c11430c930b969a0fbeabdbb8ed5f57044f2ba
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 10:16:03 +0800

    change: function name in order to fit double type

--- include/mul_vector.h
+++ include/mul_vector.h
@@ -3,8 +3,8 @@
 
 #include "myvector.h"
 
-int mul_vector_dot(MyVector *v1, MyVector *v2);
+int mul_vector_int_dot(MyVector *v1, MyVector *v2);
 
-MyVector *mul_vector_cross(MyVector *v1, MyVector *v2);
+MyVector *mul_vector_int_cross(MyVector *v1, MyVector *v2);
 
 #endif // MUL_VECTOR_H
