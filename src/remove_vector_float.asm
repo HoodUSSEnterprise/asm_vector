ec0c7e3b9a4598ed13d4688afc9aae9123066e89
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 23:40:46 +0800

    add:  nasm code

---
+++ src/remove_vector_float.asm
@@ -0,0 +1,116 @@
+global remove_vector_float
+section .text
+extern malloc
+extern free
+
+; bool remove_vector_float(VectorFloat *v, float removed_value);
+; rcx = v, xmm1 = removed_value
+
+remove_vector_float:
+
+    push rbx
+    push rdi
+    push rsi
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 8
+
+    movss [rsp], xmm15
+
+    mov r14, rcx ; r14 = v
+    movss xmm15, xmm1 ; xmm15 = removed_value
+    mov r13, [rcx + 8] ; r13 = v->len
+
+    xor rdi, rdi ; calc number of removed_value
+    mov rsi, [r14] ; rsi = v->data
+    xor rcx, rcx ; i = 0
+
+calc_number:
+    cmp rcx, r13 ; i < len
+    jge next
+    movss xmm0, [rsi + rcx * 4]
+    ucomiss xmm0, xmm15 ; judge xmm0 and elem
+    je count_plus
+
+    inc rcx
+    jmp calc_number
+
+count_plus:
+    add rdi, 1 ; number + 1
+    inc rcx
+    jmp calc_number
+
+next:
+    cmp rdi, 0
+    je no
+    cmp rdi, r13
+    je zero
+
+    ; malloc new res->data
+    mov rcx, r13
+    sub rcx, rdi
+    shl rcx, 2
+    call malloc
+    test rax, rax
+    jz failed_data
+
+    mov rbx, rax ; rbx = new_data
+
+    xor rcx, rcx ; i = 0
+    xor rdx, rdx ; j = 0
+
+change_value:
+    cmp rcx, r13 ; i < len
+    jge yes
+    movss xmm2, [rsi + rcx * 4]
+    ucomiss xmm2, xmm15 ; judge xmm0 and elem
+    je change
+
+    movss xmm0, [rsi + rcx * 4]
+    movss [rbx + rdx * 4], xmm0
+    inc rdx ; j++
+    inc rcx ; i++
+    jmp change_value
+
+change:
+    inc rcx ; j++
+    jmp change_value
+
+no:
+    mov rax, 0 ; return false
+    jmp pop_data
+
+yes:
+    mov rcx, [r14]
+    call free
+    mov [r14], rbx
+    mov rcx, r13
+    sub rcx, rdi
+    mov [r14 + 8], rcx
+    jmp pop_data
+
+zero:
+    mov rcx, [r14]
+    call free
+    mov qword [r14], 0
+    mov qword [r14 + 8], 0 ; zreo vector means len = 0
+    mov rax, 1 ; return true
+    jmp pop_data
+
+failed_data:
+    mov rax, 0
+    jmp pop_data
+
+pop_data:
+    movss xmm15, [rsp]
+    add rsp, 8
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rsi
+    pop rdi
+    pop rbx
+    ret
