2e9063d13f0df798631c15a0cc451a9a0d0ddbde
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 23:39:33 +0800

    add: new type VectorFloat and function

--- include/push_back_vector.h
+++ include/push_back_vector.h
@@ -5,6 +5,6 @@
 
 void push_back_int(VectorInt *v, int data);
 
-void push_back_double(VectorDouble *v, double data);
+void push_back_float(VectorFloat *v, float data);
 
 #endif // PUSH_BACK_VECTOR_H
