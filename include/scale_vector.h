f5b7fb51ab1b884c1179808da2bb5f650dc3a317
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 23:39:34 +0800

    add: new type VectorFloat and function

--- include/scale_vector.h
+++ include/scale_vector.h
@@ -5,6 +5,6 @@
 
 VectorInt *scale_vector_int(VectorInt *v, int scale);
 
-VectorDouble *scale_vector_double(VectorDouble *v, double scale);
+VectorFloat *scale_vector_float(VectorFloat *v, float scale);
 
 #endif // SCALE_VECTOR_H
