e8ac8975b4103d5380a54c203aa1bf05177a9805
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 11:20:17 +0800

    change:MyVector to VectorInt

--- include/remove_vector.h
+++ include/remove_vector.h
@@ -4,6 +4,6 @@
 #include "find_vector.h"
 #include "myvector.h"
 
-bool remove_vector(VectorInt *v, int removed_value);
+bool remove_vector_int(VectorInt *v, int removed_value);
 
 #endif // REMOVE_VECTOR_H
