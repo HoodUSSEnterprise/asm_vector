73cdf24c94cec43c9801eb27c6567f94d21b3af6
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 10:16:04 +0800

    change: function name in order to fit double type

--- src/pop_vector.asm
+++ src/pop_vector.asm
@@ -1,9 +1,9 @@
-global pop
+global pop_int
 section .text
 
-; void pop(MyVector *v, int *data);
+; void pop_int(MyVector *v, int *data);
 ; rcx = v, rdx = data
-pop:
+pop_int:
 
     push rbx
     push rdi
