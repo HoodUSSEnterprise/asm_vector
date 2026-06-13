16261981befc9e5272f0087f79353709aeaf48d3
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 23:39:33 +0800

    add: new type VectorFloat and function

--- include/insert_vector.h
+++ include/insert_vector.h
@@ -5,6 +5,6 @@
 
 bool insert_vector_int(VectorInt *v, size_t pos, int value);
 
-bool insert_vector_double(VectorDouble *v, size_t pos, double value);
+bool insert_vector_float(VectorFloat *v, size_t pos, float value);
 
 #endif // INSERT_VECTOR_H
