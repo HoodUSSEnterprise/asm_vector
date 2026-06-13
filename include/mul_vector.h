805323202dff9772360f9db627103ce275a9b9ef
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-13 10:18:23 +0800

    change: struct name from MyVector into VectorInt

--- include/mul_vector.h
+++ include/mul_vector.h
@@ -3,8 +3,8 @@
 
 #include "myvector.h"
 
-int mul_vector_int_dot(MyVector *v1, MyVector *v2);
+int mul_vector_int_dot(VectorInt *v1, VectorInt *v2);
 
-MyVector *mul_vector_int_cross(MyVector *v1, MyVector *v2);
+VectorInt *mul_vector_int_cross(VectorInt *v1, VectorInt *v2);
 
 #endif // MUL_VECTOR_H
