435f767d7e2db9942e8a281a5220fd3e3b376c1f
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-14 10:19:29 +0800

    format

--- include/reverse_vector.h
+++ include/reverse_vector.h
@@ -5,7 +5,8 @@
 
 void reverse_vector_int(VectorInt *v);
 
-void reverse_vector_double(VectorDouble *v);
 void reverse_vector_float(VectorFloat *v);
+
+void reverse_vector_double(VectorDouble *v);
 
 #endif // REVERSE_VECTOR_H
