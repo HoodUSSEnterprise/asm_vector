eac62aeab9371bc91b897be2d8a10666b3f16f66
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-14 10:19:28 +0800

    format

--- include/insert_vector.h
+++ include/insert_vector.h
@@ -5,7 +5,8 @@
 
 bool insert_vector_int(VectorInt *v, size_t pos, int value);
 
-bool insert_vector_double(VectorDouble *v, size_t pos, double value);
 bool insert_vector_float(VectorFloat *v, size_t pos, float value);
+
+bool insert_vector_double(VectorDouble *v, size_t pos, double value);
 
 #endif // INSERT_VECTOR_H
