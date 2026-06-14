8d00fb200c9a87cf6f820e1b3af3407d7abb4399
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-14 10:19:29 +0800

    format

--- include/replace_vector.h
+++ include/replace_vector.h
@@ -5,7 +5,8 @@
 
 bool replace_vector_int(VectorInt *v, int old_elem, int new_elem);
 
-bool replace_vector_double(VectorDouble *v, double old_elem, double new_elem);
 bool replace_vector_float(VectorFloat *v, float old_elem, float new_elem);
+
+bool replace_vector_double(VectorDouble *v, double old_elem, double new_elem);
 
 #endif // REPLACE_VECTOR_H
