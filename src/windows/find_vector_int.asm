73e40874310c1d989daa5ebbe33fcf482a4d5c3c
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-14 22:15:28 +0800

    fix: add check of v->data

--- src/windows/find_vector_int.asm
+++ src/windows/find_vector_int.asm
@@ -14,6 +14,19 @@ find_vector_int:
     push r15
 
     mov r14, rcx ; r14 = v
+
+    ; check v1
+    test r14, r14
+    jz null_ptr
+
+    mov r14, [rcx]
+
+    ; check v1->data
+    test r14, r14
+    jz null_ptr
+
+    mov r14, rcx
+
     mov r15d, edx ; r15d = elem
     mov r13, r8 ; r13 = index
 
@@ -31,6 +44,9 @@ on_loop:
     inc rcx; ; i++
     jmp on_loop
 
+null_ptr:
+    mov rax, 0
+    jmp pop_data
 
 yes:
     mov [r13], rcx
