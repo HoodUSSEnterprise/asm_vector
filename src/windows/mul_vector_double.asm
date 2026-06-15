882c99139f3abafa4a36aef2c1fc7629b4d086ce
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-15 13:05:00 +0800

    fix: add check of v->data

--- src/windows/mul_vector_double.asm
+++ src/windows/mul_vector_double.asm
@@ -24,6 +24,9 @@ mul_vector_double_dot:
     test r15, r15
     jz null_ptr_dot
 
+    mov r14, [rcx] ; r14 = v1->data
+    mov r15, [rdx] ; r15 = v2->data
+
     ; check v1->data and v2->data
     test r14, r14
     jz null_ptr_dot
@@ -89,6 +92,9 @@ mul_vector_double_cross:
     jz null_ptr_cross
     test r15, r15
     jz null_ptr_cross
+
+    mov r14, [rcx] ; r14 = v1->data
+    mov r15, [rdx] ; r15 = v2->data
 
     ; check v1->data and v2->data
     test r14, r14
