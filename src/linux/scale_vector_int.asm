540788a0636c09306f9024dbbd53c12eb80f292a
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-15 13:10:09 +0800

    fix: add check of v->data

--- src/linux/scale_vector_int.asm
+++ src/linux/scale_vector_int.asm
@@ -25,6 +25,14 @@ scale_vector_int:
     test r14, r14
     jz null_ptr
 
+    mov r14, [rdi]
+
+    ; check v->data
+    test r14, r14
+    jz pop_data
+
+    mov r14, rdi ; r14 = v
+    
     ; malloc for res, 16 byte
     mov rdi, 16
     call malloc wrt ..plt
